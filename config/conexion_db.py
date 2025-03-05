a9a89803d46320cd9fe8b58daa0e02b6c4520b10
luzangelacarabali <165611205+luzangelacarabali@users.noreply.github.com>
2025-03-04 20:10:28 -0500

    Update conexion_db.py

--- config/conexion_db.py
+++ config/conexion_db.py
@@ -2,10 +2,14 @@ import os
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
-# Cargar variables de entorno desde .env
 load_dotenv()
 
-# Construir la URL de conexión
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
 DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 
 def get_engine():
