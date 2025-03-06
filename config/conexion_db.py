74b4a4303ede711bb30d66baf99256b4fa9294f5
luzangelacarabali <luz_angela.carabali@uao.edu.co>
2025-03-06 14:28:55 -0500

    Actualizando conexion_db.py

--- config/conexion_db.py
+++ config/conexion_db.py
@@ -9,7 +9,7 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
-
+                    
 DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 
 def get_engine():
